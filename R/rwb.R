9dabcd39d2015b58dc05765af599df1f9320492f
Deepankar Basu <dbasu@umass.edu>
2023-03-28 19:40:26 -0400

    Deleted files

--- R/rwb.R
+++
@@ -1,9 +0,0 @@
-#' Real Wage Bundle, USA 2014
-#'
-#' Personal Consumption Expenditure from the Input Output Table for the US economy in 2014.
-#'
-#' @source <https://doi.org/10.34894/PJ2M1C>
-#' @format Consumption expenditure on the output of 53 industries.
-#' @examples
-#' data(rwb)
-"rwb"
