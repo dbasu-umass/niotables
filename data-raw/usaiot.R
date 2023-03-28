e5cef4ee09d4f4a273c64712e5c02d754e400cbc
Deepankar Basu <dbasu@umass.edu>
2023-03-28 09:34:09 -0400

    Code for creating usa io table

---
+++ data-raw/usaiot.R
@@ -0,0 +1,22 @@
+# libraries
+library(tidyverse)
+library(readxl)
+library(readr)
+
+# --- IO Data for input-output matrix
+# Source: WIOD 2016 Release
+usaiot <- read_excel(
+  path="~/MyRProjects/niotables/data-raw/USA_NIOT_nov16.xlsx",
+  sheet = "National IO-tables"
+) %>% 
+  dplyr::filter(
+    Origin=="Domestic"
+  ) %>%
+  dplyr::select(
+    -c("Description","Origin",
+       "CONS_h", "CONS_np","CONS_g","GFCF","INVEN","EXP","GO")
+  ) %>%
+  as.data.frame()
+
+write_csv(usaiot, "data-raw/usaiot.csv")
+save(sea, file = "data/usaiot.rda")
